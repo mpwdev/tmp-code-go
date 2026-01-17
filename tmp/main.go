9f08a2fa97b8f7b49ea5b08518c022e27ec2da2d
MP Web Developer <mprwdev@gmail.com>
2026-01-17 21:01:33 +0200

    iota

--- tmp/main.go
+++ tmp/main.go
@@ -3,25 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	const a float64 = 5.1
-	const b = 6.7
+	const (
+		c1 = iota
+		c2 = iota
+		c3 = iota
+	)
 
-	const c float64 = a * b
-	const str = "Hello " + "Go"
+	fmt.Println(c1, c2, c3)
 
-	const d = 5 > 10
+	const (
+		c11 = iota
+		c22
+		c33
+	)
 
-	fmt.Println(d)
+	fmt.Println(c11, c22, c33)
 
-	const x = 5
-	const y = 6.7 * 2
-	fmt.Printf("%T\n", x)
-	fmt.Printf("%T\n", y)
+	const (
+		a = iota * 2
+		b
+		c
+	)
 
-	var i int = x     // x to int
-	var j float64 = x // var j float64 = float64(x)
-	var p byte = x
+	fmt.Println(a, b, c)
 
-	fmt.Println(i, j, p)
-	fmt.Printf("i is: %T | j is: %T | p is: %T", i, j, p)
+	const (
+		x = -(iota + 2)
+		_ // skipped
+		y
+		z
+		_ // skipped
+		d
+		t
+	)
+
+	fmt.Println(x, y, z, d, t)
 }
