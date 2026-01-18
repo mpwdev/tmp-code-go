aa84ef35ab41ddd19dea2c36bc40ba8c415499c7
MP Web Developer <mprwdev@gmail.com>
2026-01-18 20:26:51 +0200

    c102

--- tmp/main.go
+++ tmp/main.go
@@ -4,14 +4,23 @@ import "fmt"
 
 func main() {
 
-	var a int
-	var b float64
-	var c bool
-	var d string
+	// var a int
+	// var b float64
+	// var c bool
+	// var d string
+
+	var (
+		a int
+		b float64
+		c bool
+		d string
+	)
 	
-	x := 20
-	y := 15.5
-	z := "Gopher!"
+	// x := 20
+	// y := 15.5
+	// z := "Gopher!"
+
+	x, y, z := 20, 15.5, "Gopher!"
 
 	fmt.Println(a, b, c, d, x, y, z)
 
