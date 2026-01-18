52e7799796687bc6bc4626071f1824f01c1fa4d4
MP Web Developer <mprwdev@gmail.com>
2026-01-18 20:35:55 +0200

    c1_04

--- tmp/main.go
+++ tmp/main.go
@@ -1,13 +1,12 @@
 package main
 
+import "fmt"
+
+var version = "3.1"
+
 func main() {
 
-	var a float64 = 7.1
-
-	x, y := true, 3.7
-
-	a, x = 5.5, false
-
-	_, _, _ = a, x, y
+	name := "Golang"
+	fmt.Println(name)
 
 }
