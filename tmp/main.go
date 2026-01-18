1249e6d90ee45168a1214d39700891cccb08ede1
MP Web Developer <mprwdev@gmail.com>
2026-01-18 14:27:08 +0200

    overflow

--- tmp/main.go
+++ tmp/main.go
@@ -5,43 +5,11 @@ import (
 )
 
 func main() {
-	// var r rune = 'f'
-	// fmt.Printf("%T\n", r)
-	// fmt.Println(r)
 
-	// array
-	var numbers = [4]int{3, 5, -9, 100}
-	fmt.Printf("%T\n", numbers)
+	var x uint8 = 255
+	x++ // overflow
+	fmt.Println(x)
 
-	// slice
-	var names = []string{"Bob", "Kate", "Dan"}
-	fmt.Printf("%T\n", names)
-
-	// map
-	balances := map[string]float64{
-		"USD": 2349.5,
-		"EUR": 6345.7,
-	}
-	fmt.Printf("%T\n", balances)
-
-	// struct
-
-	type Person struct {
-		name string
-		age  int
-	}
-
-	var you Person
-	fmt.Printf("%T\n", you)
-
-	// pointer
-	var zz int = 5
-	ptr := &zz
-	fmt.Printf("%T | %v\n", ptr, ptr)
-
-	// function
-	fmt.Printf("%T\n", f)
+	// a := int8(255 + 1)
 
 }
-
-func f() {}
