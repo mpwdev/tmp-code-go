8a6adbad911aac9940749917ba5978f6b195fdf3
MP Web Developer <mprwdev@gmail.com>
2026-01-18 19:15:50 +0200

    aliases

--- tmp/main.go
+++ tmp/main.go
@@ -1,33 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
 func main() {
 
-	s := string(99)
-	fmt.Println(s)
+	type second = uint
 
-	var myStr = fmt.Sprintf("%f", 44.2)
-	fmt.Println(myStr)
-
-	var myStr1 = fmt.Sprintf("%d", 34234)
-	fmt.Println(myStr1)
-
-	fmt.Println(string(34234))
-
-	s1 := "3.123"
-	fmt.Printf("%T\n", s1)
-
-	var f1, err = strconv.ParseFloat(s1, 64)
-	fmt.Println(f1*1.5, err)
-
-	i, err := strconv.Atoi("-50")
-	s2 := strconv.Itoa(20)
-
-	fmt.Printf("i type is %T, i value is %v\n", i, i)
-	fmt.Printf("s2 type is %T, s2 value is %v\n", s2, s2)
+	var hour second = 3600
+	fmt.Printf("Minutes in an hour: %d \n", hour/60)
 
 }
