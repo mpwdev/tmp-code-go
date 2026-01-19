df78e116349fd10355d58c77e8b508ba83686116
MP Web Developer <mprwdev@gmail.com>
2026-01-19 22:57:53 +0200

    c2_02

--- tmp/main.go
+++ tmp/main.go
@@ -4,13 +4,20 @@ import "fmt"
 
 func main() {
 
-	const daysWeek int = 7
-	const lightSpeedM = 299792458
-	const lightSpeed float64 = 299792458
-	const pi float64 = 3.14159
+	// const daysWeek int = 7
+	// const lightSpeedM = 299792458
+	// const lightSpeed float64 = 299792458
+	// const pi float64 = 3.14159
+
+	// grouped constants
+	const (
+		daysWeek   = 7
+		lightSpeed = 299792458
+		pi         = 3.14159
+	)
 
 	fmt.Println(daysWeek)
-	fmt.Println(lightSpeedM)
+	//fmt.Println(lightSpeedM)
 	fmt.Println(lightSpeed)
 	fmt.Println(pi)
 
