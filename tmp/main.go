34b2335c8c9ea2121d24bae9e5e14f441a5a7dae
MP Web Developer <mprwdev@gmail.com>
2026-01-24 19:46:48 +0200

    if else statements

--- tmp/main.go
+++ tmp/main.go
@@ -4,18 +4,11 @@ import "fmt"
 
 func main() {
 
-	x, y := 0.1, 5
-	var z float64
+	price, inStock := 100, true
+	if price > 80 {
+		fmt.Println("Expensive")
+	}
 
-	// Write the correct logical operator (&&, || , !)
-	// inside the expression so that result1 will be false and result2 will be true.
-
-	// false
-	result1 := x < z || int(x) != int(z)
-	fmt.Println(result1)
-
-	// true
-	result2 := y == 1*5 && int(z) == 0
-	fmt.Println(result2)
+	_ = inStock
 
 }
