ab9b7a0562ff15b487572285869cbc13addbec65
MP Web Developer <mprwdev@gmail.com>
2026-01-25 12:45:32 +0200

    simple if

--- tmp/main.go
+++ tmp/main.go
@@ -2,22 +2,25 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"strconv"
 )
 
 func main() {
 
-	// fmt.Println("os.Args", os.Args)
-	// fmt.Println("path", os.Args[0])
-	// fmt.Println("1st arg", os.Args[1])
-	// fmt.Println("2nd arg", os.Args[2])
-	// fmt.Println("3d arg", os.Args[3])
-	// fmt.Println("number of items in as.Args", len(os.Args))
+	i, err := strconv.Atoi("45a")
 
-	var result, err = strconv.ParseFloat(os.Args[1], 64)
-	fmt.Printf("%T\n", os.Args[1])
-	fmt.Printf("%T\n", result)
-	fmt.Printf("%T\n", err)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(i)
+	}
+
+	// simple if
+	if i, err := strconv.Atoi("20a"); err == nil {
+		fmt.Println("no error, i is:", i)
+	} else {
+		fmt.Println(err)
+		// handle the error
+	}
 
 }
