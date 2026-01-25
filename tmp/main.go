3d5ca2336fda4262ad5b454e81aadf55b8b31028
MP Web Developer <mprwdev@gmail.com>
2026-01-25 13:50:28 +0200

    for and break

--- tmp/main.go
+++ tmp/main.go
@@ -6,11 +6,26 @@ import (
 
 func main() {
 
-	for i := 0; i < 10; i++ {
-		if i%2 != 0 {
-			continue
+	// for i := 0; i < 10; i++ {
+	// 	if i%2 != 0 {
+	// 		continue
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	count := 0
+
+	for i := 0; true; i++ {
+		if i%13 == 0 {
+			fmt.Printf("%d is divisible by 13 \n", i)
+			count++
 		}
-		fmt.Println(i)
+		if count == 5 {
+			break
+		}
 	}
+
+	// code after for loop
+	fmt.Println("Code line (message) after for loop")
 
 }
