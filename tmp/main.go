644160bab4898be0c75628e9c9fd67f4d7512b65
MP Web Developer <mprwdev@gmail.com>
2026-01-25 13:43:17 +0200

    for and continue

--- tmp/main.go
+++ tmp/main.go
@@ -6,20 +6,11 @@ import (
 
 func main() {
 
-	// for i := 0; i < 10; i++ {
-	// 	fmt.Println(i)
-	// }
-
-	// for i := 0; i < 10; {
-	// 	fmt.Println(i)
-	// 	i++
-	// }
-
-	// simulate while loop with for loop
-	j := 10
-	for j >= 0 {
-		fmt.Println(j)
-		j--
+	for i := 0; i < 10; i++ {
+		if i%2 != 0 {
+			continue
+		}
+		fmt.Println(i)
 	}
 
 }
