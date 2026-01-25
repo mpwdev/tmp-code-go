5fb10f788153a42714a85bdd1c5b0e9bba45b756
MP Web Developer <mprwdev@gmail.com>
2026-01-25 12:38:59 +0200

    os args v2

--- tmp/main.go
+++ tmp/main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 
-	fmt.Println("os.Args", os.Args)
-	fmt.Println("path", os.Args[0])
-	fmt.Println("1st arg", os.Args[1])
-	fmt.Println("2nd arg", os.Args[2])
-	fmt.Println("3d arg", os.Args[3])
-	fmt.Println("number of items in as.Args", len(os.Args))
+	// fmt.Println("os.Args", os.Args)
+	// fmt.Println("path", os.Args[0])
+	// fmt.Println("1st arg", os.Args[1])
+	// fmt.Println("2nd arg", os.Args[2])
+	// fmt.Println("3d arg", os.Args[3])
+	// fmt.Println("number of items in as.Args", len(os.Args))
+
+	var result, err = strconv.ParseFloat(os.Args[1], 64)
+	fmt.Printf("%T\n", os.Args[1])
+	fmt.Printf("%T\n", result)
+	fmt.Printf("%T\n", err)
 
 }
