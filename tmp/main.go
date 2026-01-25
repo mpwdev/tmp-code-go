71e1921d95914f90074197400e8b471f19c8fdef
MP Web Developer <mprwdev@gmail.com>
2026-01-25 12:35:40 +0200

    os args v1

--- tmp/main.go
+++ tmp/main.go
@@ -8,5 +8,10 @@ import (
 func main() {
 
 	fmt.Println("os.Args", os.Args)
+	fmt.Println("path", os.Args[0])
+	fmt.Println("1st arg", os.Args[1])
+	fmt.Println("2nd arg", os.Args[2])
+	fmt.Println("3d arg", os.Args[3])
+	fmt.Println("number of items in as.Args", len(os.Args))
 
 }
