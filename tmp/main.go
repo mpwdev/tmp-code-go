b12aa42fd52cbb0359e53d8eff953abe4aa85152
MP Web Developer <mprwdev@gmail.com>
2026-01-25 14:14:35 +0200

    goto statement

--- tmp/main.go
+++ tmp/main.go
@@ -6,22 +6,12 @@ import (
 
 func main() {
 
-	outer := 19 // var
-	//_ = outer
-
-	people := [5]string{"Bob", "Dan", "Rob", "Kate", "Marry"}
-	friends := [2]string{"Kate", "Lisa"}
-
-outer: // label
-	for index, name := range people {
-		for _, friend := range friends {
-			if name == friend {
-				fmt.Printf("Found a friend %q at index %d\n", friend, index)
-				break outer
-			}
-		}
+	i := 0
+loop: // label
+	if i < 5 {
+		fmt.Println(i)
+		i++
+		goto loop
 	}
-	fmt.Println("next line code after break")
-	fmt.Println(outer) // no conflicts with label outer
 
 }
