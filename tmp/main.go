49b4e1da6d6a04f4a735e576a1b86696d26954fa
MP Web Developer <mprwdev@gmail.com>
2026-01-26 22:27:54 +0200

    array v5

--- tmp/main.go
+++ tmp/main.go
@@ -2,34 +2,19 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	numbers := [3]int{10, 20, 30}
-	fmt.Printf("%#v\n", numbers)
-
-	numbers[0] = 7
-	fmt.Printf("%#v\n", numbers)
-
-	for i, v := range numbers {
-		fmt.Println("index:", i, "value:", v)
+	grades := [3]int{
+		1: 10,
+		0: 5,
+		2: 7,
 	}
+	fmt.Println(grades)
 
-	fmt.Println(strings.Repeat("-", 15))
+	accounts := [3]int{2: 50}
+	fmt.Println(accounts)
 
-	for i := 0; i < len(numbers); i++ {
-		fmt.Println("index:", i, "value:", numbers[i])
-	}
-
-	fmt.Println(strings.Repeat("-", 15))
-
-	balances := [2][3]int{{5, 6, 7}, {8, 9, 10}}
-	fmt.Println(balances)
-
-	balances2 := [2][3]int{{5, 6, 7}, {8, 9}}
-	fmt.Println(balances2)
-
-	// balances3 := [2][3]int{{5, 6, 7}, {"8", 9}}
-	// fmt.Println(balances3)
+	names := [...]string{5: "Bob"}
+	fmt.Println(names, len(names))
 }
