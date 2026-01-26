f3c9bc90bd0ef47396f07414267258bbd6806027
MP Web Developer <mprwdev@gmail.com>
2026-01-26 21:09:07 +0200

    array v1

--- tmp/main.go
+++ tmp/main.go
@@ -2,30 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
+	var numbers [4]int
 
-	// lang := "golang"
-
-	// switch lang {
-	// case "Python":
-	// 	fmt.Println("it is Python")
-	// case "Go", "golang":
-	// 	fmt.Println("It is Go")
-	// default:
-	// 	fmt.Println("Other langs")
-	// }
-
-	hour := time.Now().Hour()
-	//fmt.Println(hour)
-
-	switch true {
-	case hour < 12:
-		fmt.Println("AM")
-	case hour > 12:
-		fmt.Println("PM")
-	}
+	fmt.Printf("%v\n", numbers)
+	fmt.Printf("%#v\n", numbers)
 
 }
