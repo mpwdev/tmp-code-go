88d94093bcf4351910265343d7c143f872b28666
MP Web Developer <mprwdev@gmail.com>
2026-01-26 20:54:07 +0200

    switch v2

--- tmp/main.go
+++ tmp/main.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	lang := "golang"
+	// lang := "golang"
 
-	switch lang {
-	case "Python":
-		fmt.Println("it is Python")
-	case "Go", "golang":
-		fmt.Println("It is Go")
-	default:
-		fmt.Println("Other langs")
+	// switch lang {
+	// case "Python":
+	// 	fmt.Println("it is Python")
+	// case "Go", "golang":
+	// 	fmt.Println("It is Go")
+	// default:
+	// 	fmt.Println("Other langs")
+	// }
+
+	hour := time.Now().Hour()
+	//fmt.Println(hour)
+
+	switch true {
+	case hour < 12:
+		fmt.Println("AM")
+	case hour > 12:
+		fmt.Println("PM")
 	}
 
 }
