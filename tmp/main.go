308b569617e3c878f12d5b26c0df3aecdfd2be87
MP Web Developer <mprwdev@gmail.com>
2026-01-26 21:19:55 +0200

    arrays v3

--- tmp/main.go
+++ tmp/main.go
@@ -10,4 +10,33 @@ func main() {
 	fmt.Printf("%v\n", numbers)
 	fmt.Printf("%#v\n", numbers)
 
+	var a1 = [4]float64{}
+	fmt.Printf("%#v\n", a1)
+
+	var a2 = [3]int{-10, 1, 100}
+	fmt.Printf("%#v\n", a2)
+
+	a3 := [4]string{"Bob", "Pol", "Kate", "Dan"}
+	fmt.Printf("%#v\n", a3)
+
+	a4 := [4]string{"x", "y"}
+	fmt.Printf("%#v\n", a4)
+
+	// a5 := [4]int{5, 10}
+	// fmt.Printf("%#v\n", a5)
+
+	//ellipsis operator
+
+	a5 := [...]int{1, -2, 5, 7, 4, 3}
+	fmt.Printf("%#v\n", a5)
+	fmt.Printf("a5 length is %d\n", len(a5))
+
+	a6 := [...]int{1,
+		2,
+		3,
+		4,
+		5, // <-- this last comma is mandatory
+	}
+	fmt.Println(a6)
+
 }
