e7148dcfbacc3110fc2d9d2a7cef140f388172ea
MP Web Developer <mprwdev@gmail.com>
2026-02-03 20:58:00 +0200

    slice bacling array v1

--- tmp/main.go
+++ tmp/main.go
@@ -5,26 +5,19 @@ import (
 )
 
 func main() {
-	a := [5]int{1, 2, 3, 4, 5}
-	b := a[1:3]
-	fmt.Println(b)
-	fmt.Printf("b: %v, type: %T, len: %d, cap: %d\n", b, b, len(b), cap(b))
+	s1 := []int{10, 20, 30, 40, 50}
+	s3, s4 := s1[0:2], s1[1:3]
+	fmt.Println(s1, s3, s4)
 
-	s1 := []int{1, 2, 3, 4, 5, 6}
-	s2 := s1[1:3]
-	fmt.Println(s2)
-
-	fmt.Println("--------------------------------")
-
-	fmt.Println(s1[2:])
-	fmt.Println(s1[:3])
-	fmt.Println(s1[:])
-	fmt.Println(s1[2:4])
-
-	s1 = append(s1[:4], 100)
+	s3[1] = 300
 	fmt.Println(s1)
+	fmt.Println(s3, s4)
 
-	s1 = append(s1[:4], 200)
-	fmt.Println(s1)
+	arr1 := [4]int{1, 2, 3, 4}
+	slice1, slice2 := arr1[0:2], arr1[1:3]
+	fmt.Println(arr1, slice1, slice2)
+
+	arr1[1] = 20
+	fmt.Println(arr1, slice1, slice2)
 
 }
