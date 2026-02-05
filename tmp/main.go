e190b650a6c1129b191befa9ab4a6836bc7e88f4
MP Web Developer <mprwdev@gmail.com>
2026-02-05 19:50:33 +0200

    strings v3

--- tmp/main.go
+++ tmp/main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	s1 := "Hello, World!"
-	fmt.Println(s1[2:5])
+	p := fmt.Println
+	result := strings.Contains("I love Go", "ov") // true
+	p(result)
 
+	result = strings.ContainsAny("success", "xy") // false - x or y not in success
+	p(result)
+
+	result = strings.ContainsRune("I love Go", 'v')
+	p(result)
+
+	n := strings.Count("cheese", "e") // 3
+	p(n)
+
+	p(strings.ToLower("Hello, World!"))
+	p(strings.ToUpper("Hello, World!"))
+
+	p("go" == "go")
+	p("Go" == "go")
+	p(strings.ToLower("GO") == strings.ToLower("go")) // not good
+
+	p(strings.EqualFold("GO", "go"))
 }
