353d592f6e917223bfdd2cbb40be07628807b3a8
MP Web Developer <mprwdev@gmail.com>
2026-02-05 19:34:39 +0200

    string slicing

--- tmp/main.go
+++ tmp/main.go
@@ -5,8 +5,7 @@ import (
 )
 
 func main() {
-	var1, var2 := 'a', 'b'
-	fmt.Printf("type of var1: %T, value of var1: %d\n", var1, var1)
-	fmt.Printf("type of var2: %T, value of var2: %d\n", var2, var2)
+	s1 := "Hello, World!"
+	fmt.Println(s1[2:5])
 
 }
