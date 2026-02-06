0f6ba62199bb37f962a98ed870643a1b51a72076
MP Web Developer <mprwdev@gmail.com>
2026-02-06 22:13:54 +0200

    maps comparison

--- tmp/main.go
+++ tmp/main.go
@@ -5,53 +5,19 @@ import (
 )
 
 func main() {
-	var employes map[string]string
-	fmt.Printf("%#v\n", employes)
-	fmt.Printf("no of pairs: %d\n", len(employes))
+	a := map[string]string{"A": "X"}
+	//b := map[string]string{"B": "Y"}
+	b := map[string]string{"A": "X"}
 
-	people := map[string]float64{}
-	people["John"] = 25.5
-	people["Jane"] = 30.0
-	people["Jim"] = 35.5
+	s1 := fmt.Sprintf("%s", a)
+	s2 := fmt.Sprintf("%s", b)
+	fmt.Println(s1)
+	fmt.Println(s2)
 
-	fmt.Printf("%#v\n", people)
-	fmt.Printf("no of pairs: %d\n", len(people))
-
-	map1 := make(map[int]int)
-	map1[1] = 10
-	map1[2] = 20
-	map1[3] = 30
-	fmt.Printf("%#v\n", map1)
-	fmt.Printf("no of pairs: %d\n", len(map1))
-
-	balances := map[string]float64{
-		"USD": 1000.0,
-		"EUR": 2000.0,
-		"JPY": 3000.0,
-	}
-	fmt.Printf("%#v\n", balances)
-	fmt.Printf("no of pairs: %d\n", len(balances))
-
-	balances["USD"] = 500.0
-	balances["GBP"] = 255.0
-	fmt.Printf("%#v\n", balances)
-	fmt.Printf("no of pairs: %d\n", len(balances))
-
-	//balances["RON"] = 331.0
-
-	v, ok := balances["RON"]
-	if ok {
-		fmt.Println("RON is in the map", v)
+	if s1 == s2 {
+		fmt.Println("s1 and s2 are equal")
 	} else {
-		fmt.Println("RON is not in the map")
+		fmt.Println("s1 and s2 are not equal")
 	}
-
-	for k, v := range balances {
-		fmt.Printf("key: %s, value: %#v\n", k, v)
-	}
-
-	delete(balances, "USD")
-	fmt.Printf("%#v\n", balances)
-	fmt.Printf("no of pairs: %d\n", len(balances))
 
 }
