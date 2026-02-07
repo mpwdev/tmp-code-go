6824b51abb88ca242cf4acb710f03dd34ac23033
MP Web Developer <mprwdev@gmail.com>
2026-02-07 23:30:29 +0200

    struct create

--- tmp/main.go
+++ tmp/main.go
@@ -1,16 +1,31 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	title1, author1, year1 := "The Great Gatsby", "F. Scott Fitzgerald", 1925
+	title2, author2, year2 := "1984", "George Orwell", 1949
 
-	scanner.Scan()
-	text := scanner.Text()
-	fmt.Println("You entered:", text)
+	fmt.Println(title1, author1, year1)
+	fmt.Println(title2, author2, year2)
+	fmt.Println("--------------------------------")
 
+	type book struct {
+		title  string
+		author string
+		year   int
+	}
+
+	// type book01 struct {
+	// 	title, author string
+	// 	year          int
+	// }
+
+	book1 := book{title: title1, author: author1, year: year1}
+	book2 := book{title: title2, author: author2, year: year2}
+
+	fmt.Println(book1)
+	fmt.Println(book2)
+	fmt.Printf("Book 1: %+v\n", book1)
+	fmt.Printf("Book 2: %+v\n", book2)
 }
