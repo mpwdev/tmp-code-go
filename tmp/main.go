558e59703df08e8c09da4fade2e30f836a64a8ba
MP Web Developer <mprwdev@gmail.com>
2026-02-07 13:49:59 +0200

    files v1

--- tmp/main.go
+++ tmp/main.go
@@ -2,30 +2,59 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	friends := map[string]int{
-		"Alice":   25,
-		"Bob":     30,
-		"Charlie": 35,
+	// var newFile *os.File
+	// fmt.Printf("%T\n", newFile)
+	// var err error
+
+	// newFile, err = os.Create("a.txt")
+	// if err != nil {
+	// 	// fmt.Println("Error creating file:", err)
+	// 	// return
+	// 	//----
+	// 	// fmt.Println(err)
+	// 	// os.Exit(1)
+	// 	//----
+	// 	log.Fatal(err)
+	// }
+
+	// err = os.Truncate("a.txt", 0) // complete empty file
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// newFile.Close()
+
+	file, err := os.OpenFile("a.txt", os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
 	}
+	file.Close()
 
-	neighbors := friends
+	var fileInfo os.FileInfo
+	fileInfo, err = os.Stat("a.txt")
+	p := fmt.Println
+	p("File name:", fileInfo.Name())
+	p("File size:", fileInfo.Size())
+	p("File mode:", fileInfo.Mode())
+	p("File mod time:", fileInfo.ModTime())
+	p("Is Directory:", fileInfo.IsDir())
+	p("Is Regular:", fileInfo.Mode().IsRegular())
 
-	friends["Alice"] = 50
-	fmt.Printf("friends: %#v\n", friends)
-	fmt.Printf("neighbors: %#v\n", neighbors)
+	// oldPath := "a.txt"
+	// newPath := "aaa.txt"
+	// err = os.Rename(oldPath, newPath)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	people := make(map[string]int)
-
-	for k, v := range friends {
-		people[k] = v
+	err = os.Remove("aaa.txt")
+	if err != nil {
+		log.Fatal(err)
 	}
-	fmt.Printf("people: %#v\n", people)
-
-	people["Alice"] = 25
-	fmt.Printf("friends: %#v\n", friends)
-	fmt.Printf("people: %#v\n", people)
 
 }
