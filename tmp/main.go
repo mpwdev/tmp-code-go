4294e443964e2d17183f12c5dd07ab17b1a5ac6d
MP Web Developer <mprwdev@gmail.com>
2026-02-07 13:57:13 +0200

    file writing

--- tmp/main.go
+++ tmp/main.go
@@ -7,54 +7,17 @@ import (
 )
 
 func main() {
-	// var newFile *os.File
-	// fmt.Printf("%T\n", newFile)
-	// var err error
-
-	// newFile, err = os.Create("a.txt")
-	// if err != nil {
-	// 	// fmt.Println("Error creating file:", err)
-	// 	// return
-	// 	//----
-	// 	// fmt.Println(err)
-	// 	// os.Exit(1)
-	// 	//----
-	// 	log.Fatal(err)
-	// }
-
-	// err = os.Truncate("a.txt", 0) // complete empty file
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// newFile.Close()
-
-	file, err := os.OpenFile("a.txt", os.O_CREATE|os.O_APPEND, 0644)
+	file, err := os.OpenFile("b.txt", os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
-	file.Close()
+	defer file.Close()
 
-	var fileInfo os.FileInfo
-	fileInfo, err = os.Stat("a.txt")
-	p := fmt.Println
-	p("File name:", fileInfo.Name())
-	p("File size:", fileInfo.Size())
-	p("File mode:", fileInfo.Mode())
-	p("File mod time:", fileInfo.ModTime())
-	p("Is Directory:", fileInfo.IsDir())
-	p("Is Regular:", fileInfo.Mode().IsRegular())
-
-	// oldPath := "a.txt"
-	// newPath := "aaa.txt"
-	// err = os.Rename(oldPath, newPath)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	err = os.Remove("aaa.txt")
+	byteSlice := []byte("I learn GoLang")
+	bytesWritten, err := file.Write(byteSlice)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Printf("Bytes written: %d\n", bytesWritten)
 
 }
