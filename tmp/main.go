48d459984d8734fdc4e82d943f1470a81a4dede8
MP Web Developer <mprwdev@gmail.com>
2026-02-07 14:07:08 +0200

    file write buffer

--- tmp/main.go
+++ tmp/main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
 	"log"
 	"os"
 )
 
 func main() {
-	file, err := os.OpenFile("b.txt", os.O_CREATE|os.O_APPEND, 0644)
+	file, err := os.OpenFile("my_file.txt", os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	byteSlice := []byte("I learn GoLang")
-	bytesWritten, err := file.Write(byteSlice)
+	bufferedWriter := bufio.NewWriter(file)
+	bytesWritten, err := bufferedWriter.Write(
+		[]byte{97, 98, 99},
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Bytes written: %d\n", bytesWritten)
+	log.Printf("Bytes written to buffer (not file): %d\n", bytesWritten)
+
+	bytesAvailable := bufferedWriter.Available()
+	log.Printf("Bytes available: %d\n", bytesAvailable)
+
+	bytesWritten, err = bufferedWriter.WriteString("\nJust some text")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Bytes written to buffer (not file): %d\n", bytesWritten)
+
+	unflushedBufferSize := bufferedWriter.Buffered()
+	log.Printf("Unflushed buffer size: %d\n", unflushedBufferSize)
+
+	bufferedWriter.Flush()
 
 }
