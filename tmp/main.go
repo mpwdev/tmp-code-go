e5181c1003c09773a37f7ce1af93267dbf8eafcd
MP Web Developer <mprwdev@gmail.com>
2026-02-07 15:26:19 +0200

    read from input

--- tmp/main.go
+++ tmp/main.go
@@ -3,43 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 )
 
 func main() {
-	file, err := os.Open("test.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+	scanner := bufio.NewScanner(os.Stdin)
 
-	scanner := bufio.NewScanner(file)
-	// for scanner.Scan() {
-	// 	fmt.Println(scanner.Text())
-	// }
-
-	// success := scanner.Scan()
-	// if success == false {
-	// 	err = scanner.Err()
-	// 	if err == nil {
-	// 		log.Println("Scan completed and no error occurred")
-	// 	} else {
-	// 		log.Fatal(err)
-	// 	}
-	// }
-	// fmt.Println("First line of file:", scanner.Text())
-
-	// scanner.Scan()
-	// fmt.Println("Second line of file:", scanner.Text())
-
-	// scanner.Scan()
-
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+	scanner.Scan()
+	text := scanner.Text()
+	fmt.Println("You entered:", text)
 
 }
