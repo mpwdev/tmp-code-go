2b85e95f132d9e7cc9297ce2065c3b45f66ceab8
MP Web Developer <mprwdev@gmail.com>
2026-02-09 22:41:05 +0200

    pointers to function v3

--- tmp/main.go
+++ tmp/main.go
@@ -2,51 +2,25 @@ package main
 
 import "fmt"
 
-func changeValues(quantity int, price float64, name string, sold bool) {
-	quantity = 3
-	price = 500.4
-	name = "Mob Phone"
-	sold = false
+func changeSlice(s []int) {
+	for i := range s {
+		s[i]++
+	}
 }
 
-func changeValuesByPointer(quantity *int, price *float64, name *string, sold *bool) {
-	*quantity = 3
-	*price = 500.4
-	*name = "Mob Phone"
-	*sold = false
-}
-
-//
-type Product struct {
-	price       float64
-	productName string
-}
-
-func changeValuesByType(product Product) {
-	product.price = 300
-	product.productName = "Bicycle"
-}
-
-func changeValuesByTypeByPointer(product *Product) {
-	product.price = 500.4
-	product.productName = "Mob Phone"
+func changeMap(m map[string]int) {
+	m["a"] = 10
+	m["b"] = 20
+	m["c"] = 30
 }
 
 func main() {
-	var quantity int = 5
-	var price float64 = 300.4
-	var name string = "Laptop"
-	var sold bool = true
-	fmt.Println("Before change:", quantity, price, name, sold)
-	changeValues(quantity, price, name, sold)
-	fmt.Println("After change:", quantity, price, name, sold)
-	changeValuesByPointer(&quantity, &price, &name, &sold)
-	fmt.Println("After change by pointer:", quantity, price, name, sold)
-	//
-	gift := Product{price: 100, productName: "Watch"}
-	fmt.Println("Before change:", gift)
-	changeValuesByType(gift)
-	fmt.Println("After change:", gift)
-	changeValuesByTypeByPointer(&gift)
-	fmt.Println("After change by pointer:", gift)
+	prices := []int{100, 200, 300}
+	changeSlice(prices)
+	fmt.Println(prices)
+
+	pricesMap := map[string]int{"a": 100, "b": 200, "c": 300}
+	changeMap(pricesMap)
+	fmt.Println(pricesMap)
+
 }
