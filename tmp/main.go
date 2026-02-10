afa6d5af2d584bc81e126d677cf1c4c2ae207a69
MP Web Developer <mprwdev@gmail.com>
2026-02-10 21:29:04 +0200

    func Farengait Celsium

--- tmp/main.go
+++ tmp/main.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-const boilingF = 212.0
-
 func main() {
-	var f = boilingF
-	var c = (f - 32) * 5 / 9
-	fmt.Printf("boiling point = %g°F or %g°C\n", f, c)
-	// Output:
-	// boiling point = 212°F or 100°C
+	const freezingF, boilingF = 32.0, 212.0
+	fmt.Printf("%g°F = %g°C\n", freezingF, fToC(freezingF)) // "32°F = 0°C"
+	fmt.Printf("%g°F = %g°C\n", boilingF, fToC(boilingF))   // "212°F = 100°C"
+}
+
+func fToC(f float64) float64 {
+	return (f - 32) * 5 / 9
 }
