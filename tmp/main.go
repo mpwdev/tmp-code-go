0daf604766f2b553261b59c7688a1f6a72bd0b82
MP Web Developer <mprwdev@gmail.com>
2026-02-13 23:30:42 +0200

    goroutines v3

--- tmp/main.go
+++ tmp/main.go
@@ -2,49 +2,32 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"strings"
-	"sync"
 )
 
-func checkAndSaveBody(url string, wg *sync.WaitGroup) {
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println(err)
-		fmt.Println("Error fetching url:", url)
-	} else {
-		defer resp.Body.Close()
-		fmt.Printf("%s -> Status Code: %d\n", url, resp.StatusCode)
-		if resp.StatusCode == 200 {
-			bodyBytes, err := io.ReadAll(resp.Body)
-			file := strings.Split(url, "//")[1]
-			file += ".txt"
-			fmt.Printf("Saving response body to %s\n", file)
-			err = os.WriteFile(file, bodyBytes, 0666)
-			if err != nil {
-				fmt.Println(err)
-				fmt.Println("Error writing to file:", file)
-			}
-		}
+func factorial(n int, c chan int) {
+	f := 1
+	for i := 2; i <= n; i++ {
+		f *= i
 	}
-	wg.Done()
 
+	c <- f
 }
 
 func main() {
-	urls := []string{
-		"https://www.google.com",
-		"https://www.github.com",
+
+	ch := make(chan int)
+
+	defer close(ch)
+
+	go factorial(5, ch)
+
+	f := <-ch
+	fmt.Println("5 factorial =", f)
+
+	for i := 1; i <= 20; i++ {
+		go factorial(i, ch)
+		f := <-ch
+		fmt.Printf("Factorial of %d: %d\n", i, f)
 	}
 
-	var wg sync.WaitGroup
-	wg.Add(len(urls))
-
-	for _, url := range urls {
-		go checkAndSaveBody(url, &wg)
-		// fmt.Println(strings.Repeat("-", 20))
-	}
-	wg.Wait()
 }
