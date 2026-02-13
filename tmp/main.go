ceda85e259d553e280ff9ad9d29dceaa630a7f68
MP Web Developer <mprwdev@gmail.com>
2026-02-13 23:31:05 +0200

    goroutines v4

--- tmp/main.go
+++ tmp/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func factorial(n int, c chan int) {
@@ -28,6 +29,20 @@ func main() {
 		go factorial(i, ch)
 		f := <-ch
 		fmt.Printf("Factorial of %d: %d\n", i, f)
+	}
+
+	fmt.Println(strings.Repeat("#", 10))
+
+	for i := 5; i < 15; i++ {
+		go func(n int, c chan int) {
+			f := 1
+			for i := 2; i <= n; i++ {
+				f *= i
+			}
+
+			c <- f
+		}(i, ch)
+		fmt.Printf("Factorial of %d is %d\n", i, <-ch)
 	}
 
 }
