03b218d8683a404c6fa709a7b15acddff4094ef1
MP Web Developer <mprwdev@gmail.com>
2026-02-14 23:03:23 +0200

    main and init func v3

--- tmp/main.go
+++ tmp/main.go
@@ -8,12 +8,12 @@ func init() {
 	fmt.Println("this is init function #1")
 }
 
-func init() {
-	fmt.Println("this is init function #2")
-}
-
 func main() {
 
 	fmt.Println("this is main function")
 
+}
+
+func init() {
+	fmt.Println("this is init function #2")
 }
