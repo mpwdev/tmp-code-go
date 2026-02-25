bb538693d983b3fe3c769c3662764f7341079866
MP Web Developer <mprwdev@gmail.com>
2026-02-25 22:04:00 +0200

    simple calc

--- tmp/main.go
+++ tmp/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	fmt.Print("this is main func")
+	var investmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	fmt.Println(futureValue)
 }
