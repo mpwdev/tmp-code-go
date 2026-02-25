b006af89a8de40756d68a91cb3c052bc8b135836
MP Web Developer <mprwdev@gmail.com>
2026-02-25 22:36:30 +0200

    simple calc with user inputs

--- tmp/main.go
+++ tmp/main.go
@@ -11,8 +11,13 @@ func main() {
 	var years float64 = 10
 	expectedReturnRate := 5.5
 
-	fmt.Print("Enter Investment Amount:")
+	fmt.Print("Enter Investment Amount: ")
 	fmt.Scan(&investmentAmount)
+	fmt.Print("Enter Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+	fmt.Print("Enter Years: ")
+	fmt.Scan(&years)
+	fmt.Println("--------------------------------")
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
