729ce7da7d81dc11a712aa05ecdba5fe3c1d4aeb
MP Web Developer <mprwdev@gmail.com>
2026-02-28 21:21:42 +0200

    floats output format

--- tmp/main.go
+++ tmp/main.go
@@ -22,6 +22,7 @@ func main() {
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	// fmt.Println("Future Value: ", futureValue)
+	fmt.Println("Future Value: ", fmt.Sprintf("%.2f", futureValue))
+	fmt.Println("Future Real Value: ", fmt.Sprintf("%.2f", futureRealValue))
 }
