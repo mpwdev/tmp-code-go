7d3709d28200a6e8d67dcb2ccac7d3777cf29951
MP Web Developer <mprwdev@gmail.com>
2026-02-28 22:00:41 +0200

    use custom functions

--- tmp/main.go
+++ tmp/main.go
@@ -11,18 +11,45 @@ func main() {
 	var years float64 = 10
 	expectedReturnRate := 5.5
 
-	fmt.Print("Enter Investment Amount: ")
-	fmt.Scan(&investmentAmount)
+	// fmt.Print("Enter Investment Amount: ")
+	// fmt.Scan(&investmentAmount)
+	investmentAmount = getInvestmentAmount2()
+
 	fmt.Print("Enter Expected Return Rate: ")
 	fmt.Scan(&expectedReturnRate)
 	fmt.Print("Enter Years: ")
 	fmt.Scan(&years)
 	fmt.Println("--------------------------------")
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	//futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureValue := calcFutureValue(investmentAmount, expectedReturnRate, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	// fmt.Println("Future Value: ", futureValue)
 	fmt.Println("Future Value: ", fmt.Sprintf("%.2f", futureValue))
 	fmt.Println("Future Real Value: ", fmt.Sprintf("%.2f", futureRealValue))
+
+	fmt.Println("--------------------------------")
+	outputText("text test func outputText")
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func getInvestmentAmount() float64 {
+	fmt.Print("Enter Investment Amount: ")
+	var investmentAmount float64
+	fmt.Scan(&investmentAmount)
+	return investmentAmount
+}
+
+func getInvestmentAmount2() (investmentAmount float64) {
+	fmt.Print("Enter Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+	return investmentAmount
+}
+
+func calcFutureValue(investmentAmount float64, expectedReturnRate float64, years float64) float64 {
+	return investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 }
