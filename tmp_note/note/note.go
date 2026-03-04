4ca8234d46f9c2ce1ca080560e6912625ea6df55
MP Web Developer <mprwdev@gmail.com>
2026-03-04 23:00:13 +0200

    struct tags and json format data save

--- tmp_note/note/note.go
+++ tmp_note/note/note.go
@@ -10,22 +10,22 @@ import (
 )
 
 type Note struct {
-	title     string
-	content   string
-	createdAt time.Time
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 func (note Note) Display() {
 	fmt.Println("--------------------------------")
-	fmt.Println("Note title:", note.title)
-	fmt.Println("Note content:", note.content)
-	fmt.Println("Note created at:", note.createdAt)
+	fmt.Println("Note title:", note.Title)
+	fmt.Println("Note content:", note.Content)
+	fmt.Println("Note created at:", note.CreatedAt)
 	fmt.Println("--------------------------------")
 }
 
 func (note Note) Save() error {
-	fileName := strings.ReplaceAll(note.title, " ", "_")
-	fileName = strings.ToLower(fileName)
+	fileName := strings.ReplaceAll(note.Title, " ", "_")
+	fileName = strings.ToLower(fileName) + ".json"
 
 	json, err := json.Marshal(note)
 	if err != nil {
@@ -33,7 +33,13 @@ func (note Note) Save() error {
 		return err
 	}
 
-	return os.WriteFile(fileName, json, 0644)
+	err = os.WriteFile(fileName, json, 0644)
+	if err != nil {
+		//fmt.Println("Error writing file:", err)
+		return err
+	}
+
+	return nil
 }
 
 func New(title string, content string) (Note, error) {
@@ -41,5 +47,5 @@ func New(title string, content string) (Note, error) {
 		return Note{}, errors.New("title and content are required")
 	}
 
-	return Note{title: title, content: content, createdAt: time.Now()}, nil
+	return Note{Title: title, Content: content, CreatedAt: time.Now()}, nil
 }
