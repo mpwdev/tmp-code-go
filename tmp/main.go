e5356ee53dbf9194ece00577f0e47affede44638
MP Web Developer <mprwdev@gmail.com>
2026-03-07 15:56:43 +0200

    simple array

--- tmp/main.go
+++ tmp/main.go
@@ -3,22 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var productNames [4]string = [4]string{"Product 1", "Product 2", "Product 3", "Product 4"}
-	prices := [4]float64{10.0, 20.0, 30.0, 40.0}
-
-	fmt.Println("Product names: ", productNames)
-	fmt.Println("Prices: ", prices)
-
-	productNames[2] = "Product-3-updated"
-	fmt.Println("Product names: ", productNames)
-
-	fmt.Println("Prices:", prices[2])
-
-	featuredPrices := prices[1:]
-	featuredPrices[0] = 100.0
-	fmt.Println("Featured prices: ", featuredPrices)
-
-	fmt.Println("Prices: ", prices)
-	fmt.Println(len(featuredPrices), cap(featuredPrices))
+	hobbies := [3]string{"Reading", "Traveling", "Coding"}
+	fmt.Println("Hobbies: ", hobbies)
 
 }
