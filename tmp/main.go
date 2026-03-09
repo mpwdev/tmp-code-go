826cfa133af69e98c700531e494bc49a2137405a
MP Web Developer <mprwdev@gmail.com>
2026-03-09 21:46:55 +0200

    functions as values

--- tmp/main.go
+++ tmp/main.go
@@ -6,11 +6,20 @@ type TransformFn func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2}
 	fmt.Println("Original numbers: ", numbers)
 	doubled := transformNumbers(&numbers, double)
 	fmt.Println("Doubled numbers: ", doubled)
 	tripled := transformNumbers(&numbers, triple)
 	fmt.Println("Tripled numbers: ", tripled)
+
+	transformerFn1 := getTransformerFn(&moreNumbers)
+	transformedNumbers1 := transformNumbers(&moreNumbers, transformerFn1)
+	fmt.Println("Transformed numbers 1: ", transformedNumbers1)
+
+	transformerFn2 := getTransformerFn(&numbers)
+	transformedNumbers2 := transformNumbers(&numbers, transformerFn2)
+	fmt.Println("Transformed numbers 2: ", transformedNumbers2)
 }
 
 func transformNumbers(numbers *[]int, transform TransformFn) []int {
@@ -19,6 +28,14 @@ func transformNumbers(numbers *[]int, transform TransformFn) []int {
 		dNumbers = append(dNumbers, transform(v))
 	}
 	return dNumbers
+}
+
+func getTransformerFn(numbers *[]int) TransformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
 
 func double(number int) int {
