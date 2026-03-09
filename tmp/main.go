bc1af2560a68388358272a906c97b5d4d51c4115
MP Web Developer <mprwdev@gmail.com>
2026-03-09 21:04:33 +0200

    simple doubled function

--- tmp/main.go
+++ tmp/main.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	hobbies := [3]string{"Reading", "Traveling", "Coding"}
-	fmt.Println("Hobbies: ", hobbies)
+	numbers := []int{1, 2, 3, 4}
+	fmt.Println("Original numbers: ", numbers)
+	doubled := doubleNumbers(&numbers)
+	fmt.Println("Doubled numbers: ", doubled)
+}
 
+func doubleNumbers(numbers *[]int) []int {
+	dNumbers := []int{}
+	for _, v := range *numbers {
+		dNumbers = append(dNumbers, v*2)
+	}
+	return dNumbers
 }
