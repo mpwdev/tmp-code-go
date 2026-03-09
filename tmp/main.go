3b77c2ee3736bc311359acc43f9bf29e90e4c803
MP Web Developer <mprwdev@gmail.com>
2026-03-09 21:12:25 +0200

    functions as value

--- tmp/main.go
+++ tmp/main.go
@@ -5,14 +5,24 @@ import "fmt"
 func main() {
 	numbers := []int{1, 2, 3, 4}
 	fmt.Println("Original numbers: ", numbers)
-	doubled := doubleNumbers(&numbers)
+	doubled := transformNumbers(&numbers, double)
 	fmt.Println("Doubled numbers: ", doubled)
+	tripled := transformNumbers(&numbers, triple)
+	fmt.Println("Tripled numbers: ", tripled)
 }
 
-func doubleNumbers(numbers *[]int) []int {
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
 	for _, v := range *numbers {
-		dNumbers = append(dNumbers, v*2)
+		dNumbers = append(dNumbers, transform(v))
 	}
 	return dNumbers
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
 }
