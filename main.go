86bbe60eba3344dc61d5335c757c21ba6c4b038c
MP Web Developer <mprwdev@gmail.com>
2026-03-19 22:58:10 +0200

    misc packages examples

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"example.com/tmp/utils"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	testFunc()
+	utils.UtilTestFunc()
 }
---
+++ utils/utils.go
@@ -0,0 +1,7 @@
+package utils
+
+import "fmt"
+
+func UtilTestFunc() {
+	fmt.Println("Hello, from utils")
+}
