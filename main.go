03b7fcf07b3fe9ae2c192d1c10079d85764849c9
MP Web Developer <mprwdev@gmail.com>
2026-03-20 21:00:30 +0200

    go vars

--- main.go
+++ main.go
@@ -7,7 +7,32 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	testFunc()
+	//fmt.Println("Hello, World!")
+	//testFunc()
 	utils.UtilTestFunc()
+
+	var v int = 10
+	var f float64 = 10.5
+	var s string = "Hello, World!"
+	var b bool = true
+	// var arr [3]int = [3]int{1, 2, 3}
+	// var slice []int = []int{1, 2, 3}
+	// var map map[string]int = map[string]int{"apple": 1, "banana": 2}
+	// var struct struct {
+	// 	name string
+	// 	age int
+	// } = struct {
+	// 	name string
+	// 	age int
+	// }{"John", 30}
+	// var interface interface{} = "Hello, World!"
+	// var pointer *int = &v
+	// var function func() = func() {
+	// 	fmt.Println("Hello, World!")
+	// }
+
+	fmt.Println("v int:", v)
+	fmt.Println("f float64:", f)
+	fmt.Println("s string:", s)
+	fmt.Println("b bool:", b)
 }
