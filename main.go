d6793035b211d024808066b66712eaeedeab27c1
MP Web Developer <mprwdev@gmail.com>
2026-03-21 23:33:25 +0200

    struct example

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"example.com/tmp/utils"
 )
@@ -11,12 +12,34 @@ func main() {
 	testFunc()
 	utils.UtilTestFunc()
 
-	var age2 int = 20
+	p1 := Person{
+		Name:      "Jim",
+		Age:       35,
+		Email:     "jim@example.com",
+		createdAt: time.Now(),
+	}
 
-	var age *int
-	//fmt.Println("age", age)
-	age = &age2
-	fmt.Println("age", age)
-	fmt.Println("age", *age)
-	//fmt.Println("age2", age2)
+	p1.SayHello()
+	printPerson(&p1)
+
+}
+
+// Person struct
+
+type Person struct {
+	Name      string
+	Email     string
+	Age       int
+	createdAt time.Time
+}
+
+func (p Person) SayHello() {
+	fmt.Println("Hello, my name is", p.Name, "and I am", p.Age, "years old and my email is", p.Email)
+}
+
+func printPerson(p *Person) {
+	fmt.Println("name:", p.Name)
+	fmt.Println("age:", p.Age)
+	fmt.Println("email:", p.Email)
+	fmt.Println("created at:", p.createdAt.Format(time.DateTime))
 }
