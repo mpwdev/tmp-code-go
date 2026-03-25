b74a26ee300055b7451bb0fe86f7e3d457486f42
MP Web Developer <mprwdev@gmail.com>
2026-03-25 21:06:37 +0200

    custom type

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 
-	"example.com/tmp/person"
 	"example.com/tmp/utils"
 )
 
@@ -12,33 +11,13 @@ func main() {
 	testFunc()
 	utils.UtilTestFunc()
 
-	// p1 := person.Person{
-	// 	Name:      "Jim",
-	// 	Age:       35,
-	// 	Email:     "jim@example.com",
-	// 	CreatedAt: time.Now(),
-	// }
+	var name str = "Super Max"
+	name.log()
 
-	p1, err := person.New("Jim", "jim@example.com", 35)
-	if err != nil {
-		fmt.Println("Error creating person:", err)
-		return
-	}
+}
 
-	p1.SayHello()
-	person.PrintPerson(p1)
-	p1.ClearName()
-	person.PrintPerson(p1)
+type str string
 
-	fmt.Println("--------------------------------")
-
-	a1, err := person.NewAdmin("John", "john@example.com", 27, 5)
-	if err != nil {
-		fmt.Println("Error creating admin:", err)
-		return
-	}
-	a1.SayHello()
-	person.PrintPerson(&a1.Person)
-	fmt.Println("Admin level:", a1.AdminLevel)
-
+func (text str) log() {
+	fmt.Println(text)
 }
