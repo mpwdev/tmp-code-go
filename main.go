36884c26e07d26b287193bdffe46d26f373039c0
MP Web Developer <mprwdev@gmail.com>
2026-04-06 22:13:24 +0300

    print person2

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 		fmt.Println(key, value)
 	}
 
-	person, err := person.New("John Doe", "john.doe@example.com", 30)
+	person, err := person.New("John Doe2", "john.doe2@example.com", 30)
 	if err != nil {
 		fmt.Println(err)
 		return
