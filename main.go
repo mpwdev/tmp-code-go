ce8a94396febe795ad5420f234c334c5dcdd7f91
MP Web Developer <mprwdev@gmail.com>
2026-04-12 19:21:46 +0300

    variables at package and function level

--- main.go
+++ main.go
@@ -1,34 +1,22 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"example.com/tmp/person"
-	"example.com/tmp/utils"
-)
-
-func split(sum int) (x, y int) {
-	x = sum * 4 / 9
-	y = sum - x
-	return
-}
+var c, python, java bool
 
 func main() {
-	fmt.Println("Hello, World!")
-	testFunc()
-	utils.UtilTestFunc()
+	// fmt.Println("Hello, World!")
+	// testFunc()
+	// utils.UtilTestFunc()
 
-	// comment
+	// person, err := person.New("John Doe2", "john.doe2@example.com", 30)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println(person)
 
-	person, err := person.New("John Doe2", "john.doe2@example.com", 30)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(person)
-
-	//
-
-	fmt.Println(split(10))
+	var i int
+	fmt.Println(i, c, python, java)
 
 }
