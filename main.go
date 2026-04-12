b8bef6b330d0b38624f1a3e43202158b59be24af
MP Web Developer <mprwdev@gmail.com>
2026-04-12 19:18:46 +0300

    naked return

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 	"example.com/tmp/utils"
 )
 
-func swap(x, y string) (string, string) {
-	return y, x
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
 }
 
 func main() {
@@ -27,7 +29,6 @@ func main() {
 
 	//
 
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
+	fmt.Println(split(10))
 
 }
