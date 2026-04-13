ac811237de4d3d4769dac9161394f52dc7735265
MP Web Developer <mprwdev@gmail.com>
2026-04-13 19:04:09 +0300

    variables with initializers

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-var c, python, java bool
+var i, j int = 1, 2
 
 func main() {
 	// fmt.Println("Hello, World!")
@@ -16,7 +16,7 @@ func main() {
 	// }
 	// fmt.Println(person)
 
-	var i int
-	fmt.Println(i, c, python, java)
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
 
 }
