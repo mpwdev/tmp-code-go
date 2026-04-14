01a690ef4d15855a4f07440d1f2b112ac724b887
MP Web Developer <mprwdev@gmail.com>
2026-04-14 18:57:52 +0300

    varaible type

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
@@ -17,9 +16,7 @@ func main() {
 	// }
 	// fmt.Println(person)
 
-	var x, y int = 3, 4
-	var f float64 = math.Sqrt(float64(x*x + y*y))
-	var z uint = uint(f)
-	fmt.Println(x, y, f, z)
+	v := 5.321
+	fmt.Printf("v is of type %T\n", v)
 
 }
