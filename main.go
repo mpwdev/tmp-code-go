80138143905b6741276d3f1b9529497f9ff428e7
MP Web Developer <mprwdev@gmail.com>
2026-04-14 19:09:44 +0300

    if and else

--- main.go
+++ main.go
@@ -2,7 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	return lim
+}
 
 func main() {
 	// fmt.Println("Hello, World!")
@@ -16,10 +27,9 @@ func main() {
 	// }
 	// fmt.Println(person)
 
-	sum := 1
-	for sum < 100 {
-		sum += sum
-	}
-	fmt.Println(sum)
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
 
 }
