c67cbedb670adab907b5a1ffe5c692f54422406b
MP Web Developer <mprwdev@gmail.com>
2026-04-16 18:57:57 +0300

    switch without condition

--- main.go
+++ main.go
@@ -28,20 +28,14 @@ func main() {
 	// }
 	// fmt.Println(person)
 
-	fmt.Println("When's Saturday?")
-	today := time.Now().Weekday()
-	switch time.Saturday {
-	case today + 0:
-		fmt.Println("Today.")
-	case today + 1:
-		fmt.Println("Tomorrow.")
-	case today + 2:
-		fmt.Println("In two days.")
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
 	default:
-		fmt.Println("Too far away.")
+		fmt.Println("Good evening.")
 	}
-
-	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
-	fmt.Println(time.Now().Weekday() + 1)
 
 }
