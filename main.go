4294258c58205841b907bb686e890a87611770c9
MP Web Developer <mprwdev@gmail.com>
2026-04-16 19:03:39 +0300

    pointer

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math"
-	"time"
 )
 
 func pow(x, n, lim float64) float64 {
@@ -27,15 +26,15 @@ func main() {
 	// 	return
 	// }
 	// fmt.Println(person)
+	i, j := 42, 2701
 
-	t := time.Now()
-	switch {
-	case t.Hour() < 12:
-		fmt.Println("Good morning!")
-	case t.Hour() < 17:
-		fmt.Println("Good afternoon.")
-	default:
-		fmt.Println("Good evening.")
-	}
+	p := &i         // point to i
+	fmt.Println(*p) // read i through the pointer
+	*p = 21         // set i through the pointer
+	fmt.Println(i)  // see the new value of i
+
+	p = &j         // point to j
+	*p = *p / 37   // divide j through the pointer
+	fmt.Println(j) // see the new value of j
 
 }
