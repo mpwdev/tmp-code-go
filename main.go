b2816c6e64ebc5de8de3a59c60a3ccb4782855ce
MP Web Developer <mprwdev@gmail.com>
2026-04-17 21:24:45 +0300

    slice

--- main.go
+++ main.go
@@ -27,12 +27,19 @@ func main() {
 	// }
 	// fmt.Println(person)
 
-	var a [2]string
-	a[0] = "Hello"
-	a[1] = "World"
-	fmt.Println(a[0], a[1])
-	fmt.Println(a)
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
 
-	primes := [6]int{2, 3, 5, 7, 11, 13}
-	fmt.Println(primes)
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
 }
