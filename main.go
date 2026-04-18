72e8e4beb30205468bb949ca6808bf5d191864b0
MP Web Developer <mprwdev@gmail.com>
2026-04-18 12:57:30 +0300

    range example

--- main.go
+++ main.go
@@ -27,22 +27,9 @@ func main() {
 	// }
 	// fmt.Println(person)
 
-	var s []int
-	printSlice(s)
+	var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
 
-	// append works on nil slices.
-	s = append(s, 0)
-	printSlice(s)
-
-	// The slice grows as needed.
-	s = append(s, 1)
-	printSlice(s)
-
-	// We can add more than one element at a time.
-	s = append(s, 2, 3, 4)
-	printSlice(s)
-}
-
-func printSlice(s []int) {
-	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
 }
