faa9e2d5df3e9f4abdc6c0355161c8effb6c7c21
MP Web Developer <mprwdev@gmail.com>
2026-04-19 18:16:37 +0300

    map literals

--- main.go
+++ main.go
@@ -8,7 +8,19 @@ type Vertex struct {
 	Lat, Long float64
 }
 
-var m map[string]Vertex
+var m = map[string]Vertex{
+	"Bell Labs": Vertex{
+		40.68433, -74.39967,
+	},
+	"Google": Vertex{
+		37.42202, -122.08408,
+	},
+}
+
+var m2 = map[string]Vertex{
+	"Bell Labs": {40.68433, -74.39967},
+	"Google":    {37.42202, -122.08408},
+}
 
 func main() {
 	// fmt.Println("Hello, World!")
@@ -22,10 +34,6 @@ func main() {
 	// }
 	// fmt.Println(person)
 
-	m = make(map[string]Vertex)
-	m["Bell Labs"] = Vertex{
-		40.68433, -74.39967,
-	}
-	fmt.Println(m["Bell Labs"])
 	fmt.Println(m)
+	fmt.Println(m2)
 }
