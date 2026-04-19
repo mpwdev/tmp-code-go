820e66742cffb4077cb1b99ae175ca84436f2ab7
MP Web Developer <mprwdev@gmail.com>
2026-04-19 18:19:19 +0300

    mutating maps

--- main.go
+++ main.go
@@ -4,24 +4,6 @@ import (
 	"fmt"
 )
 
-type Vertex struct {
-	Lat, Long float64
-}
-
-var m = map[string]Vertex{
-	"Bell Labs": Vertex{
-		40.68433, -74.39967,
-	},
-	"Google": Vertex{
-		37.42202, -122.08408,
-	},
-}
-
-var m2 = map[string]Vertex{
-	"Bell Labs": {40.68433, -74.39967},
-	"Google":    {37.42202, -122.08408},
-}
-
 func main() {
 	// fmt.Println("Hello, World!")
 	// testFunc()
@@ -34,6 +16,17 @@ func main() {
 	// }
 	// fmt.Println(person)
 
-	fmt.Println(m)
-	fmt.Println(m2)
+	m := make(map[string]int)
+
+	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
+
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
+
+	delete(m, "Answer")
+	fmt.Println("The value:", m["Answer"])
+
+	v, ok := m["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
 }
