5167763f89fbc1e52bb2249ceb8b40092d3f02db
MP Web Developer <mprwdev@gmail.com>
2026-04-21 20:58:19 +0300

    tmp cleanup

--- main.go
+++ main.go
@@ -1,27 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-type MyError struct {
-	When time.Time
-	What string
-}
-
-func (e *MyError) Error() string {
-	return fmt.Sprintf("at %v, %s",
-		e.When, e.What)
-}
-
-func run() error {
-	return &MyError{
-		time.Now(),
-		"it didn't work",
-	}
-}
-
 func main() {
 	// fmt.Println("Hello, World!")
 	// testFunc()
@@ -34,7 +12,4 @@ func main() {
 	// }
 	// fmt.Println(person)
 
-	if err := run(); err != nil {
-		fmt.Println(err)
-	}
 }
